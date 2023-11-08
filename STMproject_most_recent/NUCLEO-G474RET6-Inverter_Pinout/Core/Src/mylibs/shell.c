62ab0c3013a8a10810c9d8378287e6e0c885fbe4
ChatVolant <aichamendypro@gmail.com>
2023-11-08 22:51:30 +0100

    ajout de la version la plus recente du projet

---
+++ STMproject_most_recent/NUCLEO-G474RET6-Inverter_Pinout/Core/Src/mylibs/shell.c
@@ -0,0 +1,121 @@
+/*
+ * shell.c
+ *
+ *  Created on: Oct 1, 2023
+ *      Author: nicolas
+ */
+#include "usart.h"
+#include "mylibs/shell.h"
+#include <stdio.h>
+#include <string.h>
+#include "tim.h"
+#include "stdlib.h"
+
+uint8_t prompt[]="user@Nucleo-STM32G474RET6>>";
+uint8_t started[]=
+		"\r\n*-----------------------------*"
+		"\r\n| Welcome on Nucleo-STM32G474 |"
+		"\r\n*-----------------------------*"
+		"\r\n";
+uint8_t newline[]="\r\n";
+uint8_t backspace[]="\b \b";
+uint8_t cmdNotFound[]="Command not found\r\n";
+uint8_t brian[]="Brian is in the kitchen\r\n";
+uint8_t uartRxReceived;
+uint8_t uartRxBuffer[UART_RX_BUFFER_SIZE];
+uint8_t uartTxBuffer[UART_TX_BUFFER_SIZE];
+uint8_t alpha_speed; //compris en 0 et 100
+
+
+char	 	cmdBuffer[CMD_BUFFER_SIZE];
+int 		idx_cmd;
+char* 		argv[MAX_ARGS];
+int		 	argc = 0;
+char*		token;
+int 		newCmdReady = 0;
+
+void Shell_Init(void){
+	memset(argv, NULL, MAX_ARGS*sizeof(char*));
+	memset(cmdBuffer, NULL, CMD_BUFFER_SIZE*sizeof(char));
+	memset(uartRxBuffer, NULL, UART_RX_BUFFER_SIZE*sizeof(char));
+	memset(uartTxBuffer, NULL, UART_TX_BUFFER_SIZE*sizeof(char));
+
+	HAL_UART_Receive_IT(&huart2, uartRxBuffer, UART_RX_BUFFER_SIZE);
+	HAL_UART_Transmit(&huart2, started, strlen((char *)started), HAL_MAX_DELAY);
+	HAL_UART_Transmit(&huart2, prompt, strlen((char *)prompt), HAL_MAX_DELAY);
+}
+
+void Shell_Loop(void){
+	if(uartRxReceived){
+		switch(uartRxBuffer[0]){
+		case ASCII_CR: // Nouvelle ligne, instruction à traiter
+			HAL_UART_Transmit(&huart2, newline, sizeof(newline), HAL_MAX_DELAY);
+			cmdBuffer[idx_cmd] = '\0';
+			argc = 0;
+			token = strtok(cmdBuffer, " ");
+			while(token!=NULL){
+				argv[argc++] = token;
+				token = strtok(NULL, " ");
+			}
+			idx_cmd = 0;
+			newCmdReady = 1;
+			break;
+		case ASCII_BACK: // Suppression du dernier caractère
+			cmdBuffer[idx_cmd--] = '\0';
+			HAL_UART_Transmit(&huart2, backspace, sizeof(backspace), HAL_MAX_DELAY);
+			break;
+
+		default: // Nouveau caractère
+			cmdBuffer[idx_cmd++] = uartRxBuffer[0];
+			HAL_UART_Transmit(&huart2, uartRxBuffer, UART_RX_BUFFER_SIZE, HAL_MAX_DELAY);
+		}
+		uartRxReceived = 0;
+	}
+
+	if(newCmdReady){
+		if(strcmp(argv[0],"WhereisBrian?")==0){
+			HAL_UART_Transmit(&huart2, brian, sizeof(brian), HAL_MAX_DELAY);
+		}
+		else if(strcmp(argv[0],"help")==0){
+			int uartTxStringLength = snprintf((char *)uartTxBuffer, UART_TX_BUFFER_SIZE, "Print all available functions here\r\n");
+			HAL_UART_Transmit(&huart2, uartTxBuffer, uartTxStringLength, HAL_MAX_DELAY);
+		}
+
+		else if(strcmp(argv[0],"speed")==0){
+			alpha_speed=atoi(argv[1]);
+
+			if (alpha_speed<=100)
+			{
+
+			TIM1->CCR1=(alpha_speed*(TIM1->ARR))/100; //CCR1 car channel 1
+			TIM1->CCR2=(TIM1->ARR) - (TIM1->CCR1); //car complémentaire
+			}
+		}
+
+		else if(strcmp(argv[0],"start")==0){
+			TIM1->CCR1=(50*(TIM1->ARR))/100; //on fixe le rapport cyclique à 50%
+			TIM1->CCR2=(50*(TIM1->ARR))/100; //on fixe le rapport cyclique à 50%
+
+			HAL_TIM_PWM_Start (&htim1, TIM_CHANNEL_1);
+			HAL_TIMEx_PWMN_Start (&htim1, TIM_CHANNEL_1);
+			HAL_TIM_PWM_Start (&htim1, TIM_CHANNEL_2);
+			HAL_TIMEx_PWMN_Start (&htim1, TIM_CHANNEL_2);
+		}
+
+		else if(strcmp(argv[0],"stop")==0){
+			 HAL_TIM_PWM_Stop (&htim1, TIM_CHANNEL_1);
+				}
+
+		else{
+			HAL_UART_Transmit(&huart2, cmdNotFound, sizeof(cmdNotFound), HAL_MAX_DELAY);
+		}
+		HAL_UART_Transmit(&huart2, prompt, sizeof(prompt), HAL_MAX_DELAY);
+		newCmdReady = 0;
+
+	}
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef * huart){
+	uartRxReceived = 1;
+	HAL_UART_Receive_IT(&huart2, uartRxBuffer, UART_RX_BUFFER_SIZE);
+}
